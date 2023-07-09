0eed71b8f6fa49d1220803d07775ee3fb29f9597
Pushkar Deshmukh <pushkar2192@gmail.com>
2023-07-09 17:38:34 +0530

    CoreData - Todo list demo

---
+++ CoreData-Demo/ToDoListItem/ToDoListItem+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  ToDoListItem+CoreDataClass.swift
+//  CoreData-Demo
+//
+//  Created by Pushkar Deshmukh on 09/07/23.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(ToDoListItem)
+public class ToDoListItem: NSManagedObject {
+
+}
--- CoreData-Demo/ViewController.swift
+++ CoreData-Demo/ViewController.swift
@@ -8,12 +8,132 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    
+    let tableView: UITableView = {
+        let table = UITableView()
+        table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        return table
+    }()
+    
+    private var models = [ToDoListItem]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        title = "CoreData - To Do List"
+        view.addSubview(tableView)
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.frame = view.bounds
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(didTapAdd))
+        
+        getAllItems()
     }
-
-
+    
+    @objc func didTapAdd() {
+        let alert = UIAlertController(title: "New Item", message: "Enter new item", preferredStyle: .alert)
+        alert.addTextField(configurationHandler: nil)
+        alert.addAction(UIAlertAction(title: "Submit", style: .cancel, handler: { [weak self] _ in
+            guard let field = alert.textFields?.first, let text = field.text, !text.isEmpty else { return }
+            
+            self?.createItem(name: text)
+            
+        }))
+        present(alert, animated: true)
+    }
+    
+    // Core Data
+    
+    func getAllItems() {
+        do {
+            models = try context.fetch(ToDoListItem.fetchRequest())
+            
+            DispatchQueue.main.async { [weak self] in
+                self?.tableView.reloadData()
+            }
+            
+        } catch {
+            // error
+        }
+    }
+    
+    func createItem(name: String) {
+        let newItem = ToDoListItem(context: context)
+        newItem.name = name
+        newItem.createdAt = Date()
+        
+        do {
+            try context.save()
+            getAllItems()
+        } catch {
+            
+        }
+    }
+    
+    func deleteItem(item: ToDoListItem) {
+        context.delete(item)
+        
+        do {
+            try context.save()
+            getAllItems()
+        } catch {
+            
+        }
+        
+    }
+    
+    func update(item: ToDoListItem, newName: String) {
+        item.name = newName
+        
+        do {
+            try context.save()
+            getAllItems()
+        } catch {
+            
+        }
+        
+    }
 }
 
+
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return models.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let model = models[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = model.name
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        let item = models[indexPath.row]
+        
+        let sheet = UIAlertController(title: "Edit", message: nil, preferredStyle: .actionSheet)
+        
+        sheet.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        sheet.addAction(UIAlertAction(title: "Edit", style: .default, handler: { [weak self] _ in
+            let alert = UIAlertController(title: "Edit", message: "Edit item", preferredStyle: .alert)
+            alert.addTextField()
+            alert.addAction(UIAlertAction(title: "Edit", style: .cancel, handler: { [weak self] _ in
+                guard let field = alert.textFields?.first, let text = field.text, !text.isEmpty else { return }
+                
+                self?.update(item: item, newName: text)
+            }))
+            
+            self?.present(alert, animated: true)
+        }))
+        
+        sheet.addAction(UIAlertAction(title: "Delete", style: .default, handler: { [weak self] _ in
+            self?.deleteItem(item: item)
+        }))
+        present(sheet, animated: true)
+    }
+}
